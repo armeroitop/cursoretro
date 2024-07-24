7ac3d21e17b13ee5e14476a0850c1b9378483e12
David G <armeroitop@gmail.com>
2024-07-24 21:05:53 +0200

    clase5

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,42 @@
-extern "C"
-{
+extern "C" {
 #include "tinyPTC/tinyptc.h"
 }
+#define COLOR_ROJO 0x00FF0000;
+#define COLOR_AZUL 0x000000FF;
+#include <cstdint>
 
-int main()
-{
-    ptc_open("windows", 640, 360);
-    for (;;)
-        ;
+int main() {
+  uint32_t screen[640 * 360];
+  uint32_t sprite[60 * 40];
 
-    ptc_close();
+  ptc_open("windows", 640, 360);
 
-    return 0;
+  for (;;) {
+    for (uint32_t i = 0; i < 360 * 640; i++) {
+      screen[i] = COLOR_ROJO;
+    }
+
+    for (uint32_t i = 0; i < (0 + 60); ++i) {
+      for (uint32_t j = 0; j < (0 + 40); ++j) {
+        sprite[(i * 40) + j] = COLOR_AZUL;
+      }
+    }
+
+    uint32_t* p_screen = screen;
+    uint32_t* p_sprite = sprite;
+    for (uint32_t i = 0; i < (0 + 60); ++i) {
+      for (uint32_t j = 0; j < (0 + 40); ++j) {
+        *p_screen = *p_sprite;
+        ++p_screen;
+        ++p_sprite;
+      }
+      p_screen += 640 - 40;
+    }
+
+    ptc_update(screen);
+  }
+
+  ptc_close();
+
+  return 0;
 }
