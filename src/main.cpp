04bb53dc77160c50ecd248fd8a6697c1fbc899b5
David Gerardo <armeroitop@gmail.com>
2024-07-28 17:02:20 +0200

    clase9

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 extern "C" {
     #include <tinyPTC/src/tinyptc.h>
 }
-
+//#include <X11/Xlib.h>
 #include <cstdint>
+#include <iostream>
+#include "main.hpp"
 
 constexpr uint32_t KR { 0x00FF0000 };
 constexpr uint32_t KG { 0x0000FF00 };
@@ -17,19 +19,30 @@ constexpr uint32_t sprite[8 * 8] = {
 constexpr uint32_t KSCREEN_WIDTH { 640 };
 constexpr uint32_t KSCREEN_HEIGHT { 360 };
 
-uint32_t* screen { nullptr };
 
-int main() {
-    screen = new uint32_t[KSCREEN_WIDTH * KSCREEN_HEIGHT];
+
+struct Screen_t {
+    Screen_t(uint32_t w, uint32_t h) :
+        screen(new uint32_t[w * h]) {
+    }
+    ~Screen_t() {
+        delete [] screen;
+    }
+
+    uint32_t* screen { nullptr };
+};
+
+void execute() {
+    Screen_t scr(KSCREEN_WIDTH, KSCREEN_HEIGHT);
 
     ptc_open("windows", KSCREEN_WIDTH, KSCREEN_HEIGHT);
 
     while (!ptc_process_events()) {
         for (uint32_t i = 0; i < KSCREEN_WIDTH * KSCREEN_HEIGHT; i++) {
-            screen[i] = KR;
+            scr.screen[i] = KR;
         }
 
-        uint32_t* p_screen = screen;
+        uint32_t* p_screen = scr.screen;
         const uint32_t* p_sprite = sprite;
         for (uint32_t i = 0; i < 8; ++i) {
             for (uint32_t j = 0; j < 8; ++j) {
@@ -37,15 +50,25 @@ int main() {
                 ++p_screen;
                 ++p_sprite;
             }
+            throw "chucho en vinagre";
             p_screen += 640 - 8;
         }
 
-        ptc_update(screen);
+        ptc_update(scr.screen);
+    }
+    ptc_close();
+}
+
+int main() {
+
+    try {
+        execute();
+    } catch (const std::exception& e) {
+        std::cout << e.what() << '\n';
+    } catch (const char* s) {
+        std::cout << "Capturado error: " << s << '\n';
     }
 
-    ptc_close();
-
-    delete [] screen;
 
     return 0;
 }
