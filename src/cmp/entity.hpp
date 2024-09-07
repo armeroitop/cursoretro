d0f1ae47971d947bc44e1c629d371d2f265247d0
David Gerardo <armeroitop@gmail.com>
2024-09-07 14:17:32 +0200

    clase 21

--- src/cmp/entity.hpp
+++ src/cmp/entity.hpp
@@ -2,6 +2,9 @@
 
 #include <cstdint>
 #include <vector>
+#include <string>
+#include <picoPNG/src/picopng.hpp>
+#include <fstream
 
 namespace ECS {
 
@@ -10,6 +13,27 @@ namespace ECS {
             w { _w }, h { _h } {
             sprite.resize(w * h);
         }
+
+
+        explicit Entity_t() {
+            std::vector<unsigned char> pixels;
+            unsigned long dx { w }, dh { h };
+            std::ifstream file(filename);
+
+            decodePNG(pixels, dx, dh, in_png, size_t);
+
+            
+
+            w = dx; h = dh;
+
+            // int decodePNG(std::vector<unsigned char>& out_image, unsigned long& image_width, 
+            //    unsigned long& image_height, const unsigned char* in_png, 
+            //    std::size_t in_size, bool convert_to_rgba32 = true);
+        }
+
+
+
+
 
         uint32_t x { 0 }, y { 0 };
         uint32_t w { 0 }, h { 0 };
